4233b04e09e3673b8d3e03f73eb7106514a804b9
Redover <67553709+RedoverPlayer@users.noreply.github.com>
2021-04-05 15:14:21 +0200

    Changed windows and added js for lessons

--- pronote_dark_theme_plus/content.js
+++ pronote_dark_theme_plus/content.js
@@ -1,1 +1,16 @@
 // alert("Hello from your Chrome extension!");
+function main() {
+    document.querySelectorAll('table.Cours').forEach(element => detectIfCancelled(element));
+}
+
+function detectIfCancelled(elem) {
+    if (elem.querySelectorAll('div[class="NoWrap ie-ellipsis"]').length != 0) {
+        if (elem.querySelector('div[class="NoWrap ie-ellipsis"]').innerText.trim() == "Prof. absent") {
+            elem.style.opacity = "0.4";
+        } else if (elem.querySelector('div[class="NoWrap ie-ellipsis"]').innerText.trim() == "Cours annulé") {
+            elem.style.opacity = "0.4";
+        }
+    }
+}
+
+setInterval(main, 100)
